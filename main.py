7d07e268d61a5057c0436e9a833a3bb5ac16b8da
EdmarLelisLourenco <127134241+EdmarLelisLourenco@users.noreply.github.com>
2023-07-01 18:38:30 -0300

    main.py update
    legendas

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class NpsGrafico:
         self.NPS_CORES = NPS_CORES
 
     def criando_grafico(self):
-        fig, ax = plt.subplots(figsize=(10, 2))
+        fig, ax = plt.subplots(figsize=(15, 2))
 
         for i, zona in enumerate(self.NPS_ZONAS):
             ax.barh([0], width=self.NPS_VALORES[i + 1] - self.NPS_VALORES[i], left=self.NPS_VALORES[i], color=self.NPS_CORES[i])
@@ -37,13 +37,15 @@ class NpsGrafico:
             ax.set_xticks(self.NPS_VALORES)
 
         ax.barh([0], width=.6, left=self.nps, color='black')
-
         plt.text(nps, 0, f'{nps:.2f}', ha='center', va='center', color='white', weight='bold', bbox=dict(facecolor='black'))
+
+        patches = [mapatches.Patch(color=self.NPS_CORES[i], label=self.NPS_ZONAS[i]) for i in range(len(self.NPS_ZONAS))]
+        plt.legend(handles=patches, bbox_to_anchor=(0, 1))
         plt.show()
 
 
 feedback = Feedback(pd.read_csv("arquivos/Avaliação01 - Página1.csv", delimiter=';')['Nota'], pd.read_csv("arquivos/Avaliação01 - Página1.csv", delimiter=';')['Comentario'])
 nps = AnalisandoFeedback(feedback).calcular_nps()
 
-grafico = NpsGrafico(nps, ['Critical', 'Improvement', 'Quality', 'Excellence'], [-100, 0, 50, 75, 100], ['#FF595E', '#FFCA3A', '#8AC926', '#1982C4'])
+grafico = NpsGrafico(nps, ['Crpitico', 'Aperfeiçoamento', 'Qualidade', 'Excelência'], [-100, 0, 50, 75, 100], ['#FF595E', '#FFCA3A', '#8AC926', '#1982C4'])
 grafico.criando_grafico()
