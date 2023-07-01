5dff5c15ee3d0f79866d97390920c875834b4c2f
EdmarLelisLourenco <127134241+EdmarLelisLourenco@users.noreply.github.com>
2023-07-01 18:02:25 -0300

    Update main.py
    Created a class to generate a graph

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as mapatches
 
 
 class Feedback:
@@ -18,7 +20,22 @@ class AnalisandoFeedback:
         return ((promoters/len(self.feedback.nota)*100) - (detractors/len(self.feedback.nota)*100))
 
 
+class NpsGrafico:
+    def __init__(self, nps, NPS_ZONAS, NPS_VALORES, NPS_CORES):
+        self.nps = nps
+        self.NPS_ZONAS = NPS_ZONAS
+        self.NPS_VALORES = NPS_VALORES
+        self.NPS_CORES = NPS_CORES
+
+    def criando_grafico(self):
+        fig, ax = plt.subplots(figsize=(10, 2))
+
+        for i, zona in enumerate(self.NPS_ZONAS):
+            ax.barh([0], width=self.NPS_VALORES[i + 1] - self.NPS_VALORES[i], left=self.NPS_VALORES[i], color=self.NPS_CORES[i])
+
+        ax.barh([0], width=1.2, left=self.nps, color='black')
+        plt.show()
+
+
 feedback = Feedback(pd.read_csv("arquivos/Avaliação01 - Página1.csv", delimiter=';')['Nota'], pd.read_csv("arquivos/Avaliação01 - Página1.csv", delimiter=';')['Comentario'])
 nps = AnalisandoFeedback(feedback).calcular_nps()
-
-print(nps)
